a21c80e7e8f84740ace53f75b4ac5c215d384349
Daniel Downes <dd@danieldownes.co.uk>
2022-06-02 01:06:27 +0100

    Log tweak

--- DelayTest.ps1
+++ DelayTest.ps1
@@ -1,12 +1,11 @@
-$Logfile = "Logs\proc_$env:computername.log"
+$Logfile = "proc_$env:computername.log"
 function WriteLog
 {
     Param ([string]$LogString)
     $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
     $LogMessage = "$Stamp $LogString"
-    Add-content $LogFile -value $LogMessage
+    Add-content -Path $LogFile -value $LogMessage
 }
-New-Item -Path . -Name "Logs" -ItemType "directory"
 WriteLog "STARTing..."
 Start-Sleep 10
 WriteLog "Time is UP!!!"
