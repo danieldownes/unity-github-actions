0ad20020e2e520c3b3b4a87b857d67fbcc2fbdbf
Daniel Downes <dd@danieldownes.co.uk>
2022-06-02 02:05:42 +0100

    MyInvocation.MyCommand.Path

--- DelayTest.ps1
+++ DelayTest.ps1
@@ -1,4 +1,7 @@
+$mypath = $MyInvocation.MyCommand.Path
+Set-Location (Split-Path -Path $mypath)
 $Logfile = (Get-Location).Path + "\Logs\wait.log"
+
 function WriteLog
 {
     Param ([string]$LogString)
@@ -6,6 +9,9 @@ function WriteLog
     $LogMessage = "$Stamp $LogString"
     Add-content -Path $LogFile -value $LogMessage
 }
+New-Item -Path Logs/ -Name "from_script.txt" -ItemType "file" -Value "This is a text string."
 WriteLog "STARTing..."
 Start-Sleep 10
 WriteLog "Time is UP!!!"
+New-Item -Path Logs/ -Name "after_delay.txt" -ItemType "file" -Value "This is a text string."
+
