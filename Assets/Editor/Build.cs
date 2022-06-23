62093204d92e6743ad21fb8dbfc448d4458f7053
Daniel Downes <dd@danieldownes.co.uk>
2022-06-23 13:43:53 +0100

    readd build method

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -4,6 +4,17 @@ using UnityEngine.UI;
 
 public class Build 
 {
+    [MenuItem("BUILD/Windows Build")]
+    public static void Invoke()
+    {
+        string[] levels = new string[] { "Assets/Scenes/SampleScene.unity" };
+
+        BuildPipeline.BuildPlayer(levels, "Build/App.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // Copy a file from the project folder to the build folder, alongside the built game.
+        //FileUtil.CopyFileOrDirectory("Assets/Templates/Readme.txt", path + "Readme.txt");
+    }
+
     [MenuItem("BUILD/Update Version")]
     public static void UpdateVersion(string version)
     {
